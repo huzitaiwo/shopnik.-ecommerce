5da45500cdb898fdb497601b373384d0b54371ea
huzitaiwo <taiwohusayn5@gmail.com>
2021-11-23 15:10:02 +0100

    implemented tabs content funtionality

--- script.js
+++ script.js
@@ -11,4 +11,40 @@ burgerMenu.addEventListener('click', () => {
     } else {
         burgerMenuIcon.src = './icons/hamburger.svg';
     }
-})
+});
+
+// tabs component
+class Tabs {
+    constructor(container) {
+        this.container = container;
+        this.tabs = container.querySelectorAll('.trigger');
+    }
+    init() {
+        this.tabs.forEach(tab => {
+            tab.addEventListener('click', e => {
+                console.log('clicked')
+                this.toggleTabs(e);
+                this.toggleContents(e);
+            });
+        });
+    }    
+    toggleTabs(e) {
+        e.preventDefault();
+        // remove current active classes
+        this.tabs.forEach(tab => tab.classList.remove('active'));
+        // add active class to clicked
+        e.target.classList.add('active');
+    }
+    toggleContents(e) {
+        // remove active classes from content
+        this.container.querySelectorAll('.tabs-content').forEach(item => item.classList.remove('active'));
+        // add new active class to content
+        const selector = e.target.getAttribute('data-target');
+        const content = this.container.querySelector(selector);
+        content.classList.add('active');
+
+    }
+}
+
+const tabs = new Tabs(document.querySelector('.sales-tabs'));
+tabs.init();
