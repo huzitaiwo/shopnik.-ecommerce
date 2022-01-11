749563aef52557431a3074a12fe3d81450028970
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-11 15:56:22 +0100

    creating preview hover effect

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const websiteImages = document.querySelectorAll('img');
+const preview = document.querySelectorAll('.image-preview');
+const previewImages = document.querySelectorAll('.image-preview img');
+console.log(preview, previewImages)
 const navigation = document.querySelector('.nav-links');
 const burgerMenu = document.querySelector('.hamburger');
 const burgerMenuIcon = document.querySelector('.hamburger img');
@@ -75,7 +78,9 @@ if(document.body.id === 'main') {
     // add to favourite
     favIcons.forEach(icon => {
         icon.addEventListener('click', () => icon.src = './icons/favorite.svg')
-    })
+    });
+
+    // previewImages.forEach()
 }
 if(document.body.id === 'item') {
     const itemsTab = new Tabs(document.querySelector('.items-tab'));
