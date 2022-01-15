5c5d1b91db765253ce2a3375991054e3667a79ce
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-01-15 21:50:13 +0100

    updated item cart image size

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ if(document.body.id === 'main') {
             item.price = finalPrice;
             itemTable.innerHTML += `
                 <tr class="item-in-cart">
-                    <td><img src="${item.img}" alt=""></td>
+                    <td><img class="item-image" src="${item.img}" alt=""></td>
                     <td>$${item.price}</td>
                     <td class="amount"><button><img src="./icons/add.svg" alt=""></button>10<button><img src="./icons/add.svg" alt=""></button></td>
                     <td>$112</td>
